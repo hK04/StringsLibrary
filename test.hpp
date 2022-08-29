55eb5de621193588e93bbc8db79f5c72a47f41db
Matthew <matvey.anoshin@inbox.ru>
2022-08-29 20:46:56 +0300

    Delete test.hpp

--- test.hpp
+++
@@ -1,3 +0,0 @@
-#define print_int_output_of_test(x) {printf("Test Function: %s, Result: %d\n", #x, x);}
-#define print_char_pointer_output_of_test(x) {printf("Test Function: %s, Result: %c\n", #x, *x);}
-#define print_string_output_of_test(x) {printf("Test Function: %s, Result: %s\n", #x, x);}
